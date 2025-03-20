a96ac2eff96b95c0a733adc1547c6b0511ede0d0
Kasundi Fernando <kasundif@gmail.com>
2025-03-20 23:49:30 +0000

    PRAMUI-2: Different User Profiles

--- src/pages/dashboard/ManagerPortal/ManagerPortal.js
+++ src/pages/dashboard/ManagerPortal/ManagerPortal.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import NavigationBar from '../../../components/layout/NavigationBar';
+import Header from '../../../components/layout/Header';
 
 const ManagerPortal = () => {
-  return <div>Manager Portal Content</div>;
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  const toggleNavigationBar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex-1 flex flex-col">
+        <Header toggleNavigationBar={toggleNavigationBar} isOpen={isOpen} /> 
+    <div className="flex h-screen">
+      <NavigationBar isOpen={isOpen} toggleNavigationBar={toggleNavigationBar} />
+      </div>
+    </div>
+  );
 };
 
 export default ManagerPortal;
