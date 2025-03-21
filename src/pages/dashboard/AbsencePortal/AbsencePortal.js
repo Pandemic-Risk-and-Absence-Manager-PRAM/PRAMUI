63045af33ff5e1e984a9746799906f1a1aba171e
Kasundi Fernando <kasundif@gmail.com>
2025-03-21 02:06:08 +0000

    PRAMUUI-10: Initial dashboard pre-stylising

--- src/pages/dashboard/AbsencePortal/AbsencePortal.js
+++ src/pages/dashboard/AbsencePortal/AbsencePortal.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import NavigationBar from '../../../components/layout/NavigationBar';
 import Header from '../../../components/layout/Header';
+import { FaUser, FaCalendarAlt, FaUpload } from "react-icons/fa";
+import { BsFillFileEarmarkPlusFill } from "react-icons/bs";
+import { MdOutlineReport } from "react-icons/md";
+import { IoBook } from "react-icons/io5";
 import users from '../../../models/users.json';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const AbsencePortal = () => {
   const { dashboardType } = useParams();
@@ -13,6 +19,8 @@ const AbsencePortal = () => {
     setIsOpen(!isOpen);
   };
 
+  const remainingDays = user.totalDays - user.usedDays;
+
   return (
     <div className="flex h-screen flex-col">
       <Header toggleNavigationBar={toggleNavigationBar} isOpen={isOpen} />
@@ -20,15 +28,97 @@ const AbsencePortal = () => {
       <div className="flex flex-1">
         <NavigationBar isOpen={isOpen} toggleNavigationBar={toggleNavigationBar} />
 
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col flex-1 p-4">
           {/* Welcome Back message */}
-          <div className="flex justify-center items-center mt-8">
+          <div className="flex justify-center items-center mt-8 p-5">
             <div className="text-4xl text-black" style={{ fontFamily: 'Kanit, sans-serif' }}>
               Welcome Back {user.name}!
             </div>
           </div>
-          
-          {/* TODO: Add the dashbaoard icons */}
+
+          {/* Time Balances */}
+          <div className="bg-blue-100 p-6 rounded-lg mb-8">
+            <div className="grid grid-cols-3 gap-4">
+              {/* Days Remaining */}
+              <div className="flex flex-col items-center">
+                <div className="w-20 h-20">
+                  <CircularProgressbar 
+                    value={(remainingDays / user.totalDays) * 100} 
+                    text={remainingDays} 
+                    styles={buildStyles({
+                      textSize: '20px',
+                      pathColor: '#4CAF50',
+                      textColor: '#333',
+                      trailColor: '#ddd'
+                    })} 
+                  />
+                </div>
+                <span className="mt-2" style={{ fontFamily: 'Kanit, sans-serif' }}>Days Remaining</span>
+              </div>
+
+              {/* Days Used */}
+              <div className="flex flex-col items-center">
+                <div className="w-20 h-20">
+                  <CircularProgressbar 
+                    value={(user.usedDays / user.totalDays) * 100} 
+                    text={user.usedDays} 
+                    styles={buildStyles({
+                      textSize: '20px',
+                      pathColor: '#FF9800',
+                      textColor: '#333',
+                      trailColor: '#ddd'
+                    })} 
+                  />
+                </div>
+                <span className="mt-2" style={{ fontFamily: 'Kanit, sans-serif' }}>Days Used</span>
+              </div>
+
+              {/* Sick Days */}
+              <div className="flex flex-col items-center">
+                <div className="w-20 h-20">
+                  <CircularProgressbar 
+                    value={(user.sickDays / user.totalDays) * 100} 
+                    text={user.sickDays} 
+                    styles={buildStyles({
+                      textSize: '20px',
+                      pathColor: '#E91E63',
+                      textColor: '#333',
+                      trailColor: '#ddd'
+                    })} 
+                  />
+                </div>
+                <span className="mt-2" style={{ fontFamily: 'Kanit, sans-serif' }}>Sick Days</span>
+              </div>
+            </div>
+          </div>
+
+          {/* Dashboard Icons */}
+          <div className="grid grid-cols-3 gap-4">
+            <Link to="/profile" className="bg-custom-green-27 p-4 rounded-lg flex flex-col items-center">
+              <span className="text-3xl mb-2"><FaUser /></span>
+              <span style={{ fontFamily: 'Kanit, sans-serif' }}>PROFILE</span>
+            </Link>
+            <Link to="/requests" className="bg-custom-green-27 p-4 rounded-lg flex flex-col items-center">
+              <span className="text-3xl mb-2"><BsFillFileEarmarkPlusFill /></span>
+              <span style={{ fontFamily: 'Kanit, sans-serif' }}>MY REQUESTS</span>
+            </Link>
+            <Link to="/calendar" className="bg-custom-green-27 p-4 rounded-lg flex flex-col items-center">
+              <span className="text-3xl mb-2"><FaCalendarAlt /></span>
+              <span style={{ fontFamily: 'Kanit, sans-serif' }}>CALENDAR</span>
+            </Link>
+            <Link to="/report-absence" className="bg-custom-green-27 p-4 rounded-lg flex flex-col items-center">
+              <span className="text-3xl mb-2"><MdOutlineReport /></span>
+              <span style={{ fontFamily: 'Kanit, sans-serif' }}>REPORT ABSENCE</span>
+            </Link>
+            <Link to="/upload-documents" className="bg-custom-green-27 p-4 rounded-lg flex flex-col items-center">
+              <span className="text-3xl mb-2"><FaUpload /></span>
+              <span style={{ fontFamily: 'Kanit, sans-serif' }}>UPLOAD DOCUMENTS</span>
+            </Link>
+            <Link to="/resources" className="bg-custom-green-27 p-4 rounded-lg flex flex-col items-center">
+              <span className="text-3xl mb-2"><IoBook /></span>
+              <span style={{ fontFamily: 'Kanit, sans-serif' }}>RESOURCES</span>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
